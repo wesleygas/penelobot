d7b9366296182d37b73835371226ced95394cad2
Wesley <wesleygl16@gmail.com>
2024-04-16 02:00:05 +0000

    initial commit

---
+++ penelobot/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from penelobot.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'penelobot'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'description'), glob('description/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wgas',
+    maintainer_email='wesleygl16@gmail.com',
+    description='Penelobot is a modular differential drive robot inspired on the Burguer TurtleBot3',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = penelobot.my_node:main'
+        ],
+    },
+)
