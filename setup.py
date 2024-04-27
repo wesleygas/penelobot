b8b322af10e565566451d8feaa6a51fbb726e3e8
Wesley <wesleygl16@gmail.com>
2024-04-27 16:12:58 +0000

    enable ros2 control and simulated camera

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'description'), glob('description/*.xacro')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
