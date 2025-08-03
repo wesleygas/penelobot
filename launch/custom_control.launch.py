93523c3affeb767582b1150303cfed0041169076
wesleygas <wesleygl16@gmail.com>
2025-08-02 21:11:34 -0300

    add custom motor serial controller

---
+++ launch/custom_control.launch.py
@@ -0,0 +1,74 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.actions import RegisterEventHandler, DeclareLaunchArgument
+from launch.event_handlers import OnProcessStart
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    package_name='penelobot'
+
+    launch_sensors_arg = DeclareLaunchArgument(
+        'launch_sensors',
+        default_value='false',
+        description='Set to true to launch the robot sensors'
+    )
+    launch_sensors = LaunchConfiguration('launch_sensors')
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([
+                    PathJoinSubstitution(
+                        [FindPackageShare(package_name),'launch','rsp.launch.py']
+                    )
+                ]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'false'}.items() # Set to false
+    )
+    
+    custom_controller_node = Node(
+        package=package_name,
+        executable='custom_controller', 
+        name='custom_controller',
+        output='screen',
+        parameters=[{
+            'device_port': '/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_64:E8:33:83:BA:2C-if00',
+            'baud_rate': 230400,
+            'loop_rate': 30.0,
+            'wheel_separation': 0.204,
+            'wheel_radius': 0.033,
+            'enc_counts_per_rev': 1975.0,
+            'odom_frame_id': 'odom',
+            'base_frame_id': 'base_link',
+            'motor_acceleration': 9000.0,
+            'battery_reading_period': 10.0
+        }]
+    )
+
+    twist_mux_params = PathJoinSubstitution([FindPackageShare(package_name),'config','twist_mux.yaml'])
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': False}],
+            remappings=[('/cmd_vel_out','/cmd_vel_out')]
+        )
+
+    sensors_launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare(package_name), 'launch', 'sensors.launch.py']
+            )
+        ),
+        launch_arguments={'use_sim_time': 'false'}.items(),
+        condition=IfCondition(launch_sensors)
+    )
+
+    return LaunchDescription([
+        rsp,
+        twist_mux,
+        custom_controller_node,
+        launch_sensors_arg,
+        sensors_launch_include
+    ])
